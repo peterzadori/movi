0c938073a6ad8e3415c952db99ef3103d08098ad
Peter Zádori <peter.zadori@gmail.com>
2013-08-20 18:14:03 +0200

    Fixed money column

--- src/Components/Grid/Columns/Money.php
+++ src/Components/Grid/Columns/Money.php
@@ -2,7 +2,14 @@
 
 namespace movi\Components\Grid\Columns;
 
+use LeanMapper\Entity;
+
 class Money extends Column
 {
+
+	public function render(Entity $row)
+	{
+		echo number_format($row->{$this->column}, 2, '.', '');
+	}
 
 }
