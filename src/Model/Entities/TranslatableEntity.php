fa13ee85e2904567e3fd5c60ad3ddb2667584c73
Peter Zádori <peter.zadori@gmail.com>
2013-08-21 11:56:01 +0200

    Removed ugly hacks, removed static

--- src/Model/Entities/TranslatableEntity.php
+++ src/Model/Entities/TranslatableEntity.php
@@ -5,55 +5,19 @@ namespace movi\Model\Entities;
 abstract class TranslatableEntity extends IdentifiedEntity
 {
 
-	/**
-	 * @return array|null
-	 */
 	public function getTranslatableColumns()
 	{
-		static $columns;
+		$columns = [];
 
-		if ($columns === NULL) {
-			/** @var $property \LeanMapper\Reflection\Property */
-			foreach ($this->getCurrentReflection()->getEntityProperties() as $property)
-			{
-				if ($property->hasCustomFlag('translate')) {
-					$columns[$property->getColumn()] = true;
-				}
+		/** @var $property \LeanMapper\Reflection\Property */
+		foreach ($this->getCurrentReflection()->getEntityProperties() as $property)
+		{
+			if ($property->hasCustomFlag('translate')) {
+				$columns[] = $property->getColumn();
 			}
 		}
 
-		return $columns;
-	}
-
-
-	/**
-	 * @return array
-	 */
-	public function getModifiedRowData()
-	{
-		$modified = $this->row->getModifiedData();
-		$translatableColumns = $this->getTranslatableColumns();
-
-		$data = array_diff_key($modified, $translatableColumns);
-
-		if (count($data) == 0) {
-			$data['id'] = $this->id;
-		}
-
-		// To persist
-		return $data;
-	}
-
-
-	/**
-	 * @return array
-	 */
-	public function getModifiedTranslationsData()
-	{
-		$modified = $this->row->getData();
-		$translatableColumns = $this->getTranslatableColumns();
-
-		return array_intersect_key($modified, $translatableColumns);
+		return array_flip($columns);
 	}
 
 }
