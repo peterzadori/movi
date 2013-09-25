4c9cf265c4c066eb083319b9064b40a68b2cbe56
Peter <peter.zadori@gmail.com>
2013-09-25 20:35:46 +0200

    Moved language routing logic

--- src/Application/Routers/LangRoute.php
+++ src/Application/Routers/LangRoute.php
@@ -2,7 +2,9 @@
 
 namespace movi\Application\Routers;
 
+use movi\EntityNotFound;
 use movi\Localization\Languages;
+use movi\Model\Repositories\LanguagesRepository;
 use Nette\Application\Routers\Route;
 
 class LangRoute extends Route
@@ -12,10 +14,25 @@ class LangRoute extends Route
 	private $languageMask = '[<lang>/]';
 
 
-	public function __construct($mask, $metadata = [], Languages $languages)
+	public function __construct($mask, $metadata = [], Languages $languages, LanguagesRepository $languagesRepository)
 	{
 		if (count($languages->getLanguages()) > 1) {
 			$mask = $this->languageMask . $mask;
+
+			if (!isset(Route::$styles['lang'])) {
+				Route::addStyle('lang');
+				Route::setStyleProperty('lang', Route::FILTER_IN, function ($code) use ($languages, $languagesRepository) {
+					try {
+						$row = $languagesRepository->findBy(array('[code] = %s' => $code));
+
+						$languages->setCurrent($row);
+					} catch (EntityNotFound $e) {
+
+					}
+
+					return $languages->getCurrent()->code;
+				});
+			}
 		}
 
 		parent::__construct($mask, $metadata);
